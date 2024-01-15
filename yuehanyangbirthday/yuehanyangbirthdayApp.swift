e3f74ffef4c6ce06e9d23153d3ffc545e6e9a97e
Vickers Zhu <vickerszhu@163.com>
2024-01-16 00:51:26 +0900

    Initial Commit

---
+++ yuehanyangbirthday/yuehanyangbirthdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  yuehanyangbirthdayApp.swift
+//  yuehanyangbirthday
+//
+//  Created by Vickers Zhu on 2024/1/16.
+//
+
+import SwiftUI
+
+@main
+struct yuehanyangbirthdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
